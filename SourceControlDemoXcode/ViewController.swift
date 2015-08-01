fe096b9e730f6c94656b129ea8e63c683ff57c5b
InductionSoftware <inductionsoftware@gmail.com>
2015-08-01 19:15:21 -0400

    Initial Commit

---
+++ SourceControlDemoXcode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SourceControlDemoXcode
+//
+//  Created by Jeffrey Stefan on 8/1/15.
+//  Copyright (c) 2015 Induction Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // test
+    println("in viewDidLoad")
+    // end test
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
